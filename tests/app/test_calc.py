6d44292aa817102ec7ea876e3599966002013495
joker021 <joker021@debian-BULLSEYE-live-builder-AMD64>
2022-10-15 13:28:20 +0530

    Corrected Linting

--- tests/app/test_calc.py
+++ tests/app/test_calc.py
@@ -1,10 +1,14 @@
-# Sample Calc Test
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
 from app.app import calc
+
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+    )
+
+# Sample Calc Test
+
 
 def test_add_func():
     ans = calc.add(1, 2)
